89919460d583556d9a1a18aa2ffca8cd985c91b0
Alexandre <AlexandreOSouza>
2022-09-22 16:42:38 -0300

    create use case create-place

---
+++ backend/src/use-cases/place/create-place/index.spect.ts
@@ -0,0 +1,18 @@
+import { expect, test } from "vitest";
+import { CreatePlace } from ".";
+import { Place } from "../../../entities/place";
+
+test("Create an place", () => {
+  const createPlace = new CreatePlace();
+
+  expect(
+    createPlace.execute({
+      title: "Bar SP",
+      description: "Um Bar em Sp",
+      image: "/none",
+      ctaLink: "www.google.com",
+      rating: 5.5,
+      country: "Brazil",
+    })
+  ).resolves.toBeInstanceOf(Place);
+});
---
+++ backend/src/use-cases/place/create-place/index.ts
@@ -0,0 +1,35 @@
+import { Place } from "../../../entities/place";
+
+interface Request {
+  title: string;
+  description: string;
+  image: string;
+  ctaLink: string;
+  rating: number;
+  country: string;
+}
+
+type Response = Place;
+
+export class CreatePlace {
+  // this method is responsible to create a new place
+  async execute({
+    title,
+    description,
+    image,
+    ctaLink,
+    rating,
+    country,
+  }: Request): Promise<Response> {
+    const newPlace = new Place({
+      title,
+      description,
+      image,
+      ctaLink,
+      rating,
+      country,
+    });
+
+    return newPlace;
+  }
+}
